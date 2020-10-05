8fe426221fe72eef47ef455fb21570c0881937f7
srijeex <srijeetpatil7@gmail.com>
2020-10-05 20:28:10 +0530

    Updates

---
+++ precache-manifest.e506f242813888ac1abed1c7069971b8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81ae37a75544dee5aec97d6e4359220",
+    "url": "/taskmanager/index.html"
+  },
+  {
+    "revision": "6b671a213cec84a1bac5",
+    "url": "/taskmanager/static/css/2.7c209213.chunk.css"
+  },
+  {
+    "revision": "ed3a5559fbf91293d1d8",
+    "url": "/taskmanager/static/css/main.6cc15632.chunk.css"
+  },
+  {
+    "revision": "6b671a213cec84a1bac5",
+    "url": "/taskmanager/static/js/2.1203b716.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/taskmanager/static/js/2.1203b716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3a5559fbf91293d1d8",
+    "url": "/taskmanager/static/js/main.3ed36c0c.chunk.js"
+  },
+  {
+    "revision": "5d502137cfe254d61e0a",
+    "url": "/taskmanager/static/js/runtime-main.234b9fbf.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/taskmanager/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/taskmanager/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/taskmanager/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/taskmanager/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/taskmanager/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
